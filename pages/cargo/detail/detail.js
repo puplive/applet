c9c0f99843fede27abb2059c1aec09e4ec195cdd
liguorui <136320309@qq.com>
2024-02-28 13:58:52 +0800

    1

--- pages/cargo/detail/detail.js
+++ pages/cargo/detail/detail.js
@@ -6,6 +6,10 @@ Page({
         open_id: wx.getStorageSync('openId'),
         id: '',
         detail: {},
+        lq: '0',
+        rc: '0',
+        picker_show: false,
+        refund: {}
     },
     onLoad(op) {
         if (op.id) {
@@ -42,7 +46,9 @@ Page({
                         })
                     }
                     that.setData({
-                        detail: d.data
+                        detail: d.data,
+                        lq: d.data.recibir_status,
+                        rc: d.data.status
                     })
                 } else {
                     wx.showToast({
@@ -60,47 +66,144 @@ Page({
             }
         })
     },
+    change_lq: function(){
+        if(this.data.detail.recibir_status == '0'){
+            let lq = this.data.lq == '1'? '0': '1'
+            this.setData({
+                lq: lq,
+                rc: lq == '0'? '0' :this.data.rc
+            })
+        }
+    },
+    change_rc: function(){
+        if(this.data.detail.status == '0'){
+            let rc = this.data.rc == '0'? '1': '0'
+            this.setData({
+                rc: rc,
+                lq: rc == '1'? '1': this.data.lq
+            })
+        }
+    },
+    change_cc: function(){
+        if(this.data.detail.status != '2'){
+            let rc = this.data.rc != '2'? '2': '1'
+            this.setData({
+                rc: rc,
+                lq: rc == '2'? '1': this.data.lq
+            })
+            if(rc == '2'){
+                this.refund_before()
+            }
+        }
+    },
+    close_refund: function(){
+        if(this.data.detail.status != '2'){
+            let rc = '1'
+            this.setData({
+                rc: rc,
+                lq: this.data.lq
+            })
+        }
+    },
+    refund_before: function(){
+        let that = this
+        wx.request({
+            url: that.data.host + 'worksite/car-info/refund-before/' + that.data.id,
+            method: 'GET',
+            success: function (res) {
+                let d = res.data
+                if (d.Code == 200) {
+                    that.setData({
+                        refund: d.data,
+                        picker_show: true
+                    })
+                } else {
+                    wx.showToast({
+                        title: d.msg,
+                        icon: 'none',
+                    })
+                }
+            },
+            fail: function (res) {
+                console.log(res)
+            }
+        })
+    },
+    sub_cc: function(){
+        let d = {
+                openid: this.data.open_id,
+                before_time: this.data.refund.end_time
+            };
+        if(this.data.lq != this.data.detail.recibir_status){
+            d.recibir_status = this.data.lq
+        }
+        if(this.data.rc != this.data.detail.status){
+            d.status = this.data.rc
+        }
+        this.sub(d)
+    },
+    sub: function(data){
+        let that = this
+        wx.request({
+            url: that.data.host + 'worksite/car-info/detail/' + that.data.id,
+            data: data,
+            method: 'PUT',
+            success: function (res) {
+                let d = res.data
+                wx.showToast({
+                    title: d.msg,
+                    icon: 'none',
+                })
+                if (d.Code == 200) {
+                    setTimeout(() => {
+                        wx.navigateBack()
+                    }, 1500);
+                    
+                } else {
+                    that.getData()
+                }
+
+            },
+            fail: function (res) {
+                that.getData()
+                console.log(res)
+            }
+        })
+    },
     up_status: function (e) {
         let that = this
+        if(that.data.lq == that.data.detail.recibir_status && that.data.rc == that.data.detail.status){
+            return
+        }
         let type = e.currentTarget.dataset.type,
             d = {
-                openid: that.data.open_id
+                openid: that.data.open_id,
+                // recibir_status: that.data.lq,
+                // status: that.data.rc
             },
-            msg = '';
-        if (type == '0') {
-            msg = '确定领取？'
-            d.recibir_status = 1
-        } else if (type == '1') {
-            msg = '确定入场？'
-            d.status = 1
-        } else if (type == '2') {
-            msg = '确定出场？'
-            d.status = 2
+            msg = '确定修改？';
+        if(that.data.lq != that.data.detail.recibir_status){
+            d.recibir_status = that.data.lq
         }
+        if(that.data.rc != that.data.detail.status){
+            d.status = that.data.rc
+        }
+        // if (type == '0') {
+        //     msg = '确定领取？'
+        //     d.recibir_status = 1
+        // } else if (type == '1') {
+        //     msg = '确定入场？'
+        //     d.status = 1
+        // } else if (type == '2') {
+        //     msg = '确定出场？'
+        //     d.status = 2
+        // }
         wx.showModal({
             // title: '提示',
             content: msg,
             success(res) {
                 if (res.confirm) {
-                    wx.request({
-                        url: that.data.host + 'worksite/car-info/detail/' + that.data.id,
-                        data: d,
-                        method: 'PUT',
-                        success: function (res) {
-                            let d = res.data
-                            wx.showToast({
-                                title: d.msg,
-                                icon: 'none',
-                            })
-                            if (d.Code == 200) {
-                                that.getData()
-                            } else {}
-
-                        },
-                        fail: function (res) {
-                            console.log(res)
-                        }
-                    })
+                    that.sub(d)
                 } else if (res.cancel) {
                     // console.log('用户点击取消')
                 }
